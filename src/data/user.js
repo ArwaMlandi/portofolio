236a6920ff88d2917aed9bfbecce8b7e8a736cc5
arwa <arwamlandi512@gmail.com>
2023-08-06 17:58:54 +0300

    fisrt

---
+++ src/data/user.js
@@ -0,0 +1,74 @@
+import logo from "../images/logo.png"
+const INFO = {
+	main: {
+
+		title: "Reactfolio by arwa_mlandi",
+		name: "Arwa Mlandi.",
+		email: "arwaml528@gmail.com",
+		logo: logo,
+
+		
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/ArwaMlandi",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/arwamlandi?igshid=MzRlODBiNWFlZA==",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://www.facebook.com/arwa.melande",
+	},
+
+	homepage: {
+		title: "Hello, I'm Arwa Mlandi (Web developer).",
+		description:
+			"I'm a front-end developer with experience in HTML,CSS,JAVA script,React js and tailwind and Node.js,. I have experience building scalable, secure and reliable web applications using different frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating, high quality interfaces that follow industry best practices and standards. I'm always looking for new challenges and opportunities to grow as a developer."
+	},
+
+	about: {
+		title: "I’m Arwa Mlandi. I live in Lattakia-Syria, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Sport Clinc",
+			description:
+				"This application is for advertisements for a sports club that has the latest modern sports equipment and includes the latest trainers and the most important sports programs",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/css/css.png",
+			linkText: "View Project",
+			link: "https://github.com/ArwaMlandi/SportClinc",
+		},
+
+		
+
+		{
+			title: "Makeup Shop",
+			description:
+				"his application is for skin products, as it includes the most important products with international specifications, high quality, and suitable for all skin types",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/ArwaMlandi/MakeupShop",
+		},
+
+		{
+			title: "Task Focal",
+			description:
+				"This application is a training affiliated to the FocalX Foundation",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/ArwaMlandi/Task1Focal",
+		}
+
+		
+	]
+};
+
+export default INFO;
